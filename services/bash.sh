7a636b8481254225f2df10dfb2281c17255a2e14
FAN Siqi <39702038+gessiegulugulu@users.noreply.github.com>
2024-06-13 15:12:31 +0800

    Update bash.sh

--- services/bash.sh
+++ services/bash.sh
@@ -1,5 +1,5 @@
-curl -X POST 'https://api.notion.com/v1/databases/d24783c24944442c9977e3c9faa4cc08/query' \
-  -H 'Authorization: Bearer '"secret_gfvY7u5qDp2JLLyh2Mp6u2Frjsxyc5hggWTRiDVVUzz"'' \
+curl -X POST 'https://api.notion.com/v1/databases/<Database_ID>/query' \
+  -H 'Authorization: Bearer '"<Notion_API>"'' \
   -H 'Notion-Version: 2022-06-28' \
   -H "Content-Type: application/json" \
 --data '{
