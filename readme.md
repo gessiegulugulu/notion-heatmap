e621f9c5315a4125c76b145b934c8db223ae8cc7
fsq <skyef@foxmail.com>
2024-08-09 16:01:26 +0800

    [update] pomo cacultate: created_date + last_edited_date

---
+++ readme.md
@@ -0,0 +1,32 @@
+# Notion Heatmap
+
+Generate github-like heatmaps based on the notion database, deployed in vercel
+
+Public link: [click here](https://notion-heatmap.vercel.app/)
+
+## Prepare First
+
+Environment Variables
+- NOTION_TOKEN
+- NOTION_DATABASE_ID
+
+Save the above variables in a `.env` file to run locally.
+
+Also, set them in vercel's 'project settings'/'environment variables'.
+
+## Run Locally
+
+```
+node index.js
+```
+
+## Publish using [Vercel](https://vercel.com/)
+
+balala todo
+
+---
+
+notion api reference: [click here](https://developers.notion.com/reference/intro)
+
+缺点：
+- last_edited_time 会被更新
--- services/notion.js
+++ services/notion.js
@@ -7,50 +7,60 @@ const notion = new Client({
 })
 
 const database_id = process.env.NOTION_DATABASE_ID
-const today = new Date().toISOString().slice(0, 10)
+const today = new Date().toISOString().split('T')[0]; // 获取当天日期
 console.log(today)
 
 module.exports = async function getPomo() {
 
-  const { results } = await notion.databases.query({
+  const createdResults = await notion.databases.query({
     database_id: `${database_id}`,
     filter: {
-      "timestamp": "last_edited_time",
-      "last_edited_time": {
-        "is_not_empty": true,
-        "on_and_before": today
+      "timestamp": "created_time",
+      "created_time": {
+        "is_not_empty": true, 
+        "on_or_before": today
       }
-    }
-    ,
+    },
     sorts: [{
       "timestamp": "created_time",
       "direction": "ascending"
     }]
-  })
+  });
 
+  const editedResults = await notion.databases.query({
+    database_id: `${database_id}`,
+    filter: {
+      "timestamp": "last_edited_time",
+      "last_edited_time": {
+        "is_not_empty": true, 
+        "on_or_before": today
+      }
+    },
+    sorts: [{
+      "timestamp": "last_edited_time",
+      "direction": "ascending"
+    }]
+  });
 
-  const rawPomos = results.map(page => {
-    return {
-      "date": page.properties["Last edited time"].last_edited_time,
-      "pomos": 1
+  const combinedResults = [...createdResults.results, ...editedResults.results];
+
+  const pomoMap = new Map();
+
+  combinedResults.forEach(page => {
+    const date = page.properties["Created time"]?.created_time || page.properties["Last edited time"]?.last_edited_time;
+    if (date) {
+      const dateString = new Date(date).toISOString().split('T')[0];
+      if (pomoMap.has(dateString)) {
+        pomoMap.set(dateString, pomoMap.get(dateString) + 1);
+      } else {
+        pomoMap.set(dateString, 1);
+      }
     }
-  })
+  });
 
-  // const groupByKey = (data, key) => Object.values(
-  //   data.reduce((res, item) => {
-  //     const value = item[key] // date
-  //     const existing = res[value] || { [key]: value, cumPomos: 0 }
-  //     return {
-  //       ...res,
-  //       [value]: {
-  //         ...existing,
-  //         cumPomos: existing.cumPomos + item.pomos
-  //       }
-  //     }
-  //   }, {})
-  // )
+  const rawPomos = Array.from(pomoMap.entries()).map(([date, pomos]) => {
+    return { date, pomos };
+  });
 
-  // const groupedPomos = groupByKey(rawPomos, 'date')
-
-  return rawPomos
+  return rawPomos;
 }
